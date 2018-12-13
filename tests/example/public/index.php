a488d7fbe526d3083e6c9a7e870d2dbedf329c7f
CrazyCodes <c919342864@yahoo.com>
2018-12-13 20:17:52 +0800

    update example index.php

--- tests/example/public/index.php
+++ tests/example/public/index.php
@@ -10,15 +10,47 @@
     // +----------------------------------------------------------------------
     // | Github: CrazyCodes <https://github.com/CrazyCodes>
     // +----------------------------------------------------------------------
-    $dirPath = dirname(__FILE__);
-
-    \Zero\Bootstrap::run(
-        $request = new \Zero\Zero(),
-        $dirPath);
     
     /**
-     * @return \Zero\Http\Response
+     * Bootstrap Zero to start his core content
      */
+    \Zero\Bootstrap::run(
+    
+    
+    /**
+     * In this case, an anonymous class is
+     * transferred to the startup file with the following three parameters
+     *
+     * @param $configs
+     * Stores configuration file directories and their namespaces as member variables in anonymous classes
+     *
+     * @param $modules
+     * Load the modules you need to load, not the extra ones
+     *
+     * @param $globals
+     * Load the global variables you need into them, and you can call them at any time throughout the life cycle
+     */
+        new class
+            /**
+             * @var $routeDirectory  The directory name of the route
+             * @var $configDirectory The name of the directory where the configuration file resides
+             */
+        {
+            protected $routeDirectory = "route";
+            protected $configDirectory = "config";
+        },
+        [
+            'Route',
+            'Database',
+            'View',
+        ],
+        [
+            'dirname' => dirname(__FILE__),
+        ],
+        
+        $request = new Zero\Zero
+    );
+    
     $response = $request->send();
     
     $response->end();
